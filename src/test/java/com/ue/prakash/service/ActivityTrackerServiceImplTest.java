628068a7d5418e28e46f530355fd6634b6534a4a
avikdigidev <avikdigidev@gmail.com>
2022-03-27 01:36:46 +0530

    Added service impl test skeleton

--- src/test/java/com/ue/prakash/service/ActivityTrackerServiceImplTest.java
+++ src/test/java/com/ue/prakash/service/ActivityTrackerServiceImplTest.java
@@ -1,17 +1,30 @@
 package com.ue.prakash.service;
 
+import com.ue.prakash.repository.ActivityTrackerRepository;
 import com.ue.prakash.service.impl.ActivityTrackerServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ActivityTrackerServiceImplTest {
-
+    AutoCloseable openMocks;
     @InjectMocks
     private ActivityTrackerServiceImpl underTest;
+    @Mock
+    ActivityTrackerRepository activityTrackerRepository;
+    @BeforeEach
+    public void setUp() throws Exception {
 
+        MockitoAnnotations.openMocks(this);
+    }
+    @AfterEach
+    void tearDown() throws Exception {
+        openMocks.close();
+    }
 }
