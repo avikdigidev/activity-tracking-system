c1948fcdb6eb929ed2be583df0823f0a72545970
avikdigidev <avikdigidev@gmail.com>
2022-03-29 23:56:20 +0530

    Added test cases

--- src/test/java/com/ue/prakash/controller/ActivityControllerTest.java
+++ src/test/java/com/ue/prakash/controller/ActivityControllerTest.java
@@ -2,36 +2,17 @@ package com.ue.prakash.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ue.prakash.exception.NoDataFoundException;
 import com.ue.prakash.pojo.dto.response.ActivityReportResponse;
-import com.ue.prakash.pojo.dto.response.MonthlyActivity;
-import com.ue.prakash.pojo.dto.response.TwoDayActivity;
 import com.ue.prakash.service.ActivityTrackerService;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,7 +28,7 @@ public class ActivityControllerTest {
     ActivityTrackerService activityTrackerService;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
 
         mockMvc = MockMvcBuilders.standaloneSetup(activityController).build();
     }
